765fcb4b27688b50ba7661f8c4ac3aaca12da93a
OloE0pia <lores.javier@gmail.com>
2015-03-11 18:21:47 -0400

    Added mockito and espresso testing dependencies to presentation module.

---
+++ android/presentation/src/androidTest/java/example/example/com/sportsofficial/EspressoTest.java
@@ -0,0 +1,10 @@
+package example.example.com.sportsofficial;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public EspressoTest() {
+        super(MainActivity.class);
+    }
+}
